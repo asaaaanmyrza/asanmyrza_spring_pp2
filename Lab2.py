b226ee4cf7839be412a59a144bf2caf0e16bf749
asAAAAAAAAAN myrza <balance@asAAAAAAAAN-myrza.local>
2024-01-23 19:23:47 +0600

    5asan5

---
+++ Lab2.py
@@ -0,0 +1,66 @@
+# Booleans 1
+
+print(10 > 9)
+#True
+
+# 2
+
+print(10 == 9)
+#False
+
+# 3
+
+print(10 < 9)
+#False
+
+# 4
+
+print(bool("abc"))
+#True
+
+# 5
+
+print(bool(0))
+#False
+
+# Operators 1
+
+print(10 * 5)
+
+# 2
+
+print(10 / 5)
+
+# 3
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+# 4
+
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+# 5
+
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+
+# Lists 1
+
+
+
+# 2
+
+
+
+# 3
+
+
+
+# 4
+
+
+
+# 5
